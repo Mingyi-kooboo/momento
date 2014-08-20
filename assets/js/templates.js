25a475ea3e2bd7ee7029fe1e857e4314dc34d34b
Mingyi-kooboo <mingyi@kooboo.com>
2014-08-20 17:39:28 +0800

    the home page 100%

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -1,8 +1,18 @@
-$(function(){
-            $(".five-box > li").click(function(){
-                $(this).siblings().children("p").hide().siblings("strong").removeClass("title-FB-hover");
-                $(this).children("p").show().siblings("strong").addClass("title-FB-hover");
-                $(this).siblings().children("strong").children("span").removeClass("icon-minus").addClass("icon-plus");
-                $(this).children("strong").children("span").removeClass("icon-plus").addClass("icon-minus");
-            });
-        });
+$(function() {
+    $(".five-box > li").click(function() {
+        $(this).siblings().children("p").hide().siblings("strong").removeClass("title-FB-hover");
+        $(this).children("p").show().siblings("strong").addClass("title-FB-hover");
+        $(this).siblings().children("strong").children("span").removeClass("icon-minus").addClass("icon-plus");
+        $(this).children("strong").children("span").removeClass("icon-plus").addClass("icon-minus");
+    });
+
+
+    $('.slider').flexslider({
+        controlNav: true,
+        directionNav: false,
+        start: function(slider) {
+            //$(slider.directionNav).appendTo('.slider-topic');
+        }
+    });
+
+});
